14f3598e930a1f8f373d1af7a6d333e9fc6e1f2d
mahendra-accolitedigital <101805723+mahi-accolite@users.noreply.github.com>
2024-08-13 13:58:39 +0530

    playlist

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
+using Odyssey.MusicMatcher;
+using SpotifyWeb;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<SpotifyService>();
+
+builder
+    .Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>()
+    .RegisterService<SpotifyService>();
+
+builder
+    .Services
+    .AddCors(options =>
+    {
+        options.AddDefaultPolicy(builder =>
+        {
+            builder
+                .WithOrigins("https://studio.apollographql.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+
+app.MapGraphQL();
 
 app.Run();
